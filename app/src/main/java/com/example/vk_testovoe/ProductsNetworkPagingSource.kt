2a52654a2a1d94af62d6212788234043d8e57ed8
nevermind322 <81963698+nevermind322@users.noreply.github.com>
2024-03-12 20:03:53 +0300

    fix pagingSource

--- app/src/main/java/com/example/vk_testovoe/ProductsNetworkPagingSource.kt
+++ app/src/main/java/com/example/vk_testovoe/ProductsNetworkPagingSource.kt
@@ -11,34 +11,28 @@ import kotlinx.coroutines.withContext
 
 data class LoadKeys(val skip: Int, val limit: Int) {
     fun getNext(): LoadKeys = LoadKeys(skip + limit, limit)
-    fun getPrev(): LoadKeys? = if (skip == 0) null else LoadKeys(skip - limit, limit)
+    fun getPrev(): LoadKeys? = if (skip < limit) null else LoadKeys(skip - limit, limit)
 }
 
 class ProductsNetworkPagingSource(private val category: String?) :
     PagingSource<LoadKeys, Product>() {
-    override fun getRefreshKey(state: PagingState<LoadKeys, Product>): LoadKeys? {
-        val anchor = state.anchorPosition ?: return null
-
-        val last = state.closestItemToPosition(anchor) ?: return null
-        return LoadKeys(last.id, state.config.pageSize)
-    }
+    override fun getRefreshKey(state: PagingState<LoadKeys, Product>): LoadKeys? = null
 
     override suspend fun load(params: LoadParams<LoadKeys>): LoadResult<LoadKeys, Product> =
         withContext(Dispatchers.IO) {
             val key = params.key ?: LoadKeys(0, 20)
             try {
-                val data = if (category == null)
-                    apiService.getProducts(key.limit, key.skip).products
+                val response = if (category == null)
+                    apiService.getProducts(key.limit, key.skip)
                 else
-                    apiService.getProductsInCategory(category, key.limit, key.skip).products
-
+                    apiService.getProductsInCategory(category, key.limit, key.skip)
+                val data = response.products
                 LoadResult.Page(
                     data = data,
                     prevKey = key.getPrev(),
-                    nextKey = if (data.isEmpty()) null else key.getNext()
+                    nextKey = if (response.skip + response.limit >= response.total) null else key.getNext()
                 )
             } catch (e: Exception) {
-                Log.d("paging", "${e.javaClass.name}: ${e.message}")
                 LoadResult.Error(e)
             }
         }
